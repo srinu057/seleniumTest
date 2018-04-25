e160c7c8ae20ab73b5acd5c69f3c60387a206956
sraovavi <sraovavi@LIN40000017.corp.capgemini.com>
2018-04-25 12:35:37 +0530

    Initial Check in

---
+++ AestAI/src/test/java/MyTest/Test1.java
@@ -0,0 +1,20 @@
+package MyTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	 public WebDriver driver;
+	@Test
+	public void login()
+	{
+		System.out.println("My Test1");
+	   
+	   System.setProperty("webdriver.chrome.driver", "C:\\backup\\Selenium\\JarFiles\\chromedriver.exe");
+	   driver = new ChromeDriver();
+	   driver.get("https://www.softwaretestingmaterial.com/");
+	}
+
+}
